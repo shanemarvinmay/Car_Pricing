0d271759f4bbde5a7b75bfd13ba8559c126837ef
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-04-07 21:47:26 -0500

    Shane setting up an outline for me

--- Microservices/index.js
+++ Microservices/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT | 3000;
 
 //firebase stuff
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDvrgVpxLNe5PfbisbAXgGvFeheFvnRwl8",
     authDomain: "senior-design-b2b93.firebaseapp.com",
     databaseURL: "https://senior-design-b2b93.firebaseio.com",
@@ -13,29 +13,45 @@ var firebaseConfig = {
     appId: "1:439726316771:web:54a2b1f38dea07774b79d6",
     measurementId: "G-RJHPMWLQEX"
 };
-var firebase = require("firebase").initializeApp(firebaseConfig);
-var db = firebase.database();
+const firebase = require("firebase").initializeApp(firebaseConfig);
+const db = firebase.database();
 
 
 app.get('/login', (req, res) => {
+    // look up user 
+    // check to make sure password matches 
+    // if password matches, then return user json obj 
     res.send('login!');
 });
 
 app.post('/signup', (req, res) => {
+    // save user data 
     res.send('signup !');
 });
 
 app.get('/forgot-password', (req, res) => {
+    // look up password 
     res.send('forgot password!');
 });
 
 app.post('/save', (req, res) => {
+    // save super important car info 
     res.send('save !');
 });
 
 app.get('/lookup', (req, res) => {
+    // get user history 
+    // return it as json 
     res.send('lookup !');
 });
+
+// end point for calc value of car 
+
+// end point for judging car 
+
+
+
+
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
 
