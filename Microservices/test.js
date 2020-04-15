5d26283ea2069e34a2fecc3d051ae269f1244756
Richarda <39577672+RikkiButler20@users.noreply.github.com>
2020-04-15 16:00:53 -0500

    Testing signup Still hasn't written to database

--- Microservices/test.js
+++ Microservices/test.js
@@ -7,11 +7,9 @@ const port = process.env.PORT || 8080;
 // signup page post request
 
 app.get('/', (req, res) => {
-    
-    
     axios({
-        method: 'get',
-        url: 'http://localhost:3000/',
+        method: 'post',
+        url: 'localhost:3000/signup?',
         data: {
             firstName: 'Richarda',
             lastName: 'Butler',
@@ -22,28 +20,13 @@ app.get('/', (req, res) => {
         }
     }).then((response) => {
         console.log(response);
-        res.send('test root end point reached');
     }, (error) => {
         console.log(error);
     });
+    console.log('hit boom');
+    res.send('User is signed up!')
 });
 
-// axios({
-//     method: 'post',
-//     url: 'localhost:3000/signup?',
-//     data: {
-//         firstName: 'Richarda',
-//         lastName: 'Butler',
-//         username: 'Rikki',
-//         password: 'password',
-//         securityQuestion: 'What is the name of my dog',
-//         securityAnswer: 'Sparky'
-//     }
-// }).then((response) => {
-//     console.log(response);
-// }, (error) => {
-//     console.log(error);
-// });
 
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
