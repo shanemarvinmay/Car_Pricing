a924df3d2270b32981cd1f60e92b008640877770
Treshawn95 <treshawn95@yahoo.com>
2020-04-22 09:52:58 -0500

    Log in Ajax

--- VERSUS 5/VERSUS/LoginViewController.swift
+++ VERSUS 5/VERSUS/LoginViewController.swift
@@ -6,48 +6,114 @@
 //  Copyright © 2020 Jazmine N Spann. All rights reserved.
 //
 
-import Foundation
-import UIKit
 
-class LoginViewController: UIViewController {
+import UIKit
+//import Foundation
+
+
+
+
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
       
 
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var passWord: UITextField!
+    @IBOutlet weak var login: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
- /*
-        passWord.addTarget(self, action: #selector(checkAndDisplayError(passWord:)), for: .editingChanged)
-    }
     
-    @objc func checkAndDisplayError (passWord: UITextField) {
-        if (passWord.text?.count ?? 0 >= 20) {
-            errorLabel.text = "Enter more than 20 characters"
-        }
-        else {
-            errorLabel.text = " "
-        }
+    override func viewDidLoad()
+    {
 
-   */
-    }
-    /*
-    //LoginUser
-    @IBAction func loginUser(_ sender: Any) {
-        
-        guard let _ = userName.text, userName.text?.characters.count != 0 else {
-            errorLabel.isHidden = false
-            errorLabel.text = "enter your email"
-            return
-        }
-        guard let _ = passWord.text, passWord.text?.characters.count != 0
-            else {
-                errorLabel.isHidden = false
-                errorLabel.text = "please enter password"
-        }
-    }
-    override func didReceiveMemoryWarning() {
-        super.viewDidLoad()
-    }
-*/
 }
+    @IBAction func signin(_ sender: Any) {
+         let alertController = UIAlertController(title: "alert ", message: "Username / Password is incorrect", preferredStyle: UIAlertController.Style.alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    present(alertController, animated: true, completion: nil)
+                guard let url = URL(string: "https://vast-gorge-25891.herokuapp.com/save-car-info?make=&quot;&quot;&model=&quot;&quot;&year=&quot;&quot;&mpg=&quot;&quot;&milage=") else { return }
+                let session = URLSession.shared
+                session.dataTask(with: url) { (data, response, error) in
+                    if let response = response {
+                        print(response)
+                    }
+                    if let data = data {
+                        print(data)
+                        }
+                    
+                }.resume()
+        
+        
+            let username =  userName.text
+            let password = passWord.text
+
+            if (username == "" || password == "")
+        {
+            return
+    }
+        //submit ( userName!, _pw: passWord!)
+}
+    //func submit (_ user:String, _pw:String)
+    //{
+     
+        //let request = NSMutableURLRequest(url: url!)
+       /* let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            if let error = error{
+            print("error: \(error)" )
+            }
+            else {
+                if let response = response as? HTTPURLResponse {
+                    print("Status: \(response.Status)")
+                }
+                if let data = data let dataString = String(data: data, encoding: utf8) {
+                    print("data: \(dataString)")
+                }
+            }
+        */
+        
+        //let request = NSMutableURLRequest(url: url!)
+        //request.httpMethod = "POST"
+        
+       // let parmtosend = "username=" + user + " &password=" + pssw
+        //request.httpBody = paramtosend.data(using: String.Encoding.utf8);
+        /*let url = URL( string: "https://vast-gorge-25891.herokuapp.com/login?username=")
+        let session = URLSession.shared
+        let request = NSMutableURLRequest(url: url!)
+        request.httpMethod = "POST"
+         let paramToSend = "username=" + user + " &password=" + _pw
+         request.httpBody = paramToSend.data(using: String.Encoding.utf8)
+        let task = session.dataTask(with: request as URLRequest, completionHandler:
+        {(data, response,error) in
+            guard let _:Data = data else
+            { return }
+            let json: Any?
+            do {json = try JSONSerialization.jsonObject( with data!, options:[] )}
+            catch {return}
+            guard let server_response = json as? NSDictionary else {
+                return
+            }
+            if let data_block = server_response["data"] as? NSDictionary{
+                if let session_data = data_block["session"] as? String
+                {
+                    let preferences = UserDefaults.standard
+                    preferences.set(session_data,forKey:"session")
+                    DispatchQueue.main.async (execute: self.submit())
+                    
+        }}
+}
+
+        
+        
+    }
+        
+        
+        task. resume(*/
+}
+
+
+
+
+
+
+
+//}
+//}
