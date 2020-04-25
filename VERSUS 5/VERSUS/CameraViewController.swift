a302a69676a3ca50f53837e8b23d6e1ce001bd97
Jspann1 <jazminespann101@gmail.com>
2020-04-25 18:59:39 -0500

    Camera View: Added POST Request: getting fatal error (need to connect button to UIIMage View)

--- VERSUS 5/VERSUS/CameraViewController.swift
+++ VERSUS 5/VERSUS/CameraViewController.swift
@@ -14,86 +14,74 @@ import AVFoundation
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
    //connections to storyboard: cameraview
-
-    @IBOutlet weak var imageView: UIImageView!
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    }
-//GET REQUEST: To get value from image --------------------------------------------------------------------------------------
-    @IBOutlet weak var valueRequest: UILabel!
-    @IBAction func picValue(_ sender: Any) {
-         let url = URL(string: " https://ml-car-value.herokuapp.com/image")
-                     guard let requestUrl = url else { fatalError() }
-                     
-                     //Create request for URL
-                     var request = URLRequest(url: requestUrl)
-                     
-                     //Specifying method to use for HTTP
-                     request.httpMethod = "POST"
-        
-                     //Send HTTP request
-                     let task = URLSession.shared.dataTask(with: request) {
-                         (data, response, error) in
-                         
-                         //Check for errors
-                         if let error = error {
-                             print ("Error took place \(error)")
-                             return
-                         }
-                     //Reading HTTP response status code
-                         if let response = response as? HTTPURLResponse {
-                             print("Response HTTP Status code: \(response.statusCode)")
-                         }
-                      //Converting the response data of HTTP to string
-                          if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                          print("Response data string: \n \(dataString)")
-                              do {
-                                  let json = try JSONSerialization.jsonObject(with: data, options: [])
-                                  print(json)
-                              } catch {
-                                  print(error)
-                              }
-                           }
-                 
-                      }
-                     task.resume()
-          }
-    
     //Switching between camera and photos------------------------------------------------------------------------------------------
-    @IBAction func SelectionBtn(_ sender: Any) {
-        
-        let imagePickrController = UIImagePickerController()
-        imagePickrController.delegate = self
-        let actionSheet = UIAlertController(title: "Photo Source", message: "choose a source", preferredStyle: .actionSheet )
-                actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
-                    if UIImagePickerController.isSourceTypeAvailable(.camera){
-                    imagePickrController.sourceType = .camera
-                    self.present(imagePickrController, animated: true, completion: nil)
-                    }
-                    else {
-                        print("Camera is Unavailable")
-                    }
+@IBAction func SelectionBtn(_ sender: Any) {
+let imagePickrController = UIImagePickerController()
+    imagePickrController.delegate = self
+let actionSheet = UIAlertController(title: "Photo Source", message: "choose a source", preferredStyle: .actionSheet )
+    actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
+if UIImagePickerController.isSourceTypeAvailable(.camera){
+    imagePickrController.sourceType = .camera
+self.present(imagePickrController, animated: true, completion: nil)
+    }
+else {
+    print("Camera is Unavailable")
+    }
+    }))
+    actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action: UIAlertAction) in
+    imagePickrController.sourceType = .photoLibrary
+self.present(imagePickrController, animated: true, completion: nil)
                 }))
-                actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action: UIAlertAction) in
-                    imagePickrController.sourceType = .photoLibrary
-                    self.present(imagePickrController, animated: true, completion: nil)
-                }))
-                actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-                self.present(actionSheet, animated: true, completion: nil)
-            }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+    actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+self.present(actionSheet, animated: true, completion: nil)
+    }
     
+    
+func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
     imageView.image = image
         picker.dismiss(animated: true, completion: nil)
     }
     
-    
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-                super.didReceiveMemoryWarning()     }
+   
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var imageView: UIImageView!
+   override func viewDidLoad() {
+   super.viewDidLoad()
 }
 
-  
+//GET REQUEST: To get value from image --------------------------------------------------------------------------------------
+ @IBOutlet weak var valueRequest: UILabel!
+ @IBAction func picValue(_ sender: UIImageView) {
+    
+//let image = valueRequest.text
+    
+let url = URL(string:" https://ml-car-value.herokuapp.com/image")
+guard let requestURL = url else { fatalError() }
+    
+var request = URLRequest(url: requestURL)
+request.httpMethod = "POST"
+
+let postString = " ";
+        
+        // Set HTTP Request Body
+request.httpBody = postString.data(using: String.Encoding.utf8);
+let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+if let error = error {
+print("Error\(error)")
+ return
+  }
+ if let data = data, let dataString = String(data: data, encoding: .utf8) {
+  print("Response given by data string:\n\(dataString)")
+  DispatchQueue.main.async {
+    let alert = UIAlertController(title: "Image Value", message: "The Estimated Price of vehicle: $\(dataString)", preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "OKAY", style: .default, handler: nil)
+        alert.addAction(okAction)
+    self.present(alert, animated: true)
+                    //self.valueRequest.text = "$\(dataString)"
+                }
+            }
+        }
+        task.resume()
+    }
+}
