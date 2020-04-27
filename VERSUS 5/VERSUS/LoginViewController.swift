6d380fe9063a47ed00b1426a159a86666ca702c2
Treshawn95 <treshawn95@yahoo.com>
2020-04-27 17:48:52 -0500

    looking into log in error

--- VERSUS 5/VERSUS/LoginViewController.swift
+++ VERSUS 5/VERSUS/LoginViewController.swift
@@ -6,17 +6,161 @@
 //  Copyright © 2020 Jazmine N Spann. All rights reserved.
 //
 
-import Foundation
-import UIKit
 
-class LoginViewController: UIViewController {
+import UIKit
+import Foundation
+
+
+
+
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
       
 
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var passWord: UITextField!
+    @IBOutlet weak var login: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+//<<<<<<< HEAD
+    
+    override func viewDidLoad()
+    {
+  super.viewDidLoad()
+}
+    
+   
+        
+        
+    @IBAction func signin(_ sender: Any) {
+        
+        let valueDisp = String ()
+        let username =  userName.text
+        let password = passWord.text
+        
+        //let url = URL(string: "https://vast-gorge-25891.herokuapp.com/login?username=\(userName))&password=\( passWord))")!
+
+        func makeGetCall() {
+          // Set up the URL request
+
+
+                
+                let url = URL(string: "https://vast-gorge-25891.herokuapp.com/login?username=\(userName))&password=\( passWord)")
+                guard let requestUrl = url else { fatalError() }
+                
+                var request = URLRequest(url: requestUrl)
+                request.httpMethod = "POST"
+                
+                let postString = "userId=300&title=My urgent task&completed=false";
+                // Set HTTP Request Body
+                request.httpBody = postString.data(using: String.Encoding.utf8);
+                // Perform HTTP Request
+                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                        
+                        // Check for Error
+                        if let error = error {
+                            print("Error took place \(error)")
+                            return
+                        }
+                 
+                        // Convert HTTP Response Data to a String
+                        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                               print("Response data string:\n \(dataString)")
+                        }
+                }
+                 task.resume()
+            }
+            // request.httpMethod = "POST"
+        //let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+           // if let error = error {
+           //     print("error: \(error)")
+            //} else {
+              //  if let response = response as? HTTPURLResponse {
+                //    print("statusCode: \(response.statusCode)")
+                //}
+                //if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                  //  print("data: \(dataString)")
+        //let task = URLSession.shared.dataTask(with: url ) { (data, response, error) in
+            //if let error = error {
+            //let alertController = UIAlertController(title: "alert ", message: "Username / Password is incorrect", preferredStyle: UIAlertController.Style.alert)
+            //alertController.addAction(UIAlertAction(title: "Try Again", style: UIAlertAction.Style.default, handler: nil))
+            // print("Username or Password \(error)")
+              //return
+         // } else {
+          //if let response = response as? HTTPURLResponse {
+            //  print("statusCode: \(response.statusCode)")
+          //}
+          //if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            //  print("Response data string: \n \(dataString)")
+            //}
+
+        
+        }
+        }
+
+        
+       
+
+           //   if (username == "" || password == "")
+       // {
+         //   return
+
+        //submit ( userName!, _pw: passWord!)
+    //func submit (_ user:String, _pw:String)
+    //{
+     
+        //let request = NSMutableURLRequest(url: url!)
+       /* let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            if let error = error{
+            print("error: \(error)" )
+            }
+            else {
+                if let response = response as? HTTPURLResponse {
+                    print("Status: \(response.Status)")
+                }
+                if let data = data let dataString = String(data: data, encoding: utf8) {
+                    print("data: \(dataString)")
+                }
+            }
+        */
+        
+        //let request = NSMutableURLRequest(url: url!)
+        //request.httpMethod = "POST"
+        
+       // let parmtosend = "username=" + user + " &password=" + pssw
+        //request.httpBody = paramtosend.data(using: String.Encoding.utf8);
+        /*let url = URL( string: "https://vast-gorge-25891.herokuapp.com/login?username=")
+        let session = URLSession.shared
+        let request = NSMutableURLRequest(url: url!)
+        request.httpMethod = "POST"
+         let paramToSend = "username=" + user + " &password=" + _pw
+         request.httpBody = paramToSend.data(using: String.Encoding.utf8)
+        let task = session.dataTask(with: request as URLRequest, completionHandler:
+        {(data, response,error) in
+            guard let _:Data = data else
+            { return }
+            let json: Any?
+            do {json = try JSONSerialization.jsonObject( with data!, options:[] )}
+            catch {return}
+            guard let server_response = json as? NSDictionary else {
+                return
+            }
+            if let data_block = server_response["data"] as? NSDictionary{
+                if let session_data = data_block["session"] as? String
+                {
+                    let preferences = UserDefaults.standard
+                    preferences.set(session_data,forKey:"session")
+                    DispatchQueue.main.async (execute: self.submit())
+                    
+        }}
+}
+
+        
+        
+    }
+        
+        
+        task. resume(*/
+  
  
       /* passWord.addTarget(self, action: #selector(checkAndDisplayError(passWord:)), for: .editingChanged)
     }
@@ -34,8 +178,8 @@ class LoginViewController: UIViewController {
  */
         
 
-    //LoginUser 
-        func loginUser(_ sender: Any) {
+    //LoginUser
+      /*  func loginUser(_ sender: Any) {
         
         guard let _ = userName.text, userName.text?.count != 0 else {
             errorLabel.isHidden = false
@@ -49,8 +193,17 @@ class LoginViewController: UIViewController {
        return
             }
     }
-        func didReceiveMemoryWarning() {
-        super.viewDidLoad()
-    }
-}
+       // func didReceiveMemoryWarning() {
+      //  super.viewDidLoad()
+   // }*/
+
+
+
+
+
+
+
+
+//}
+//}
 }
