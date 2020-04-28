46da2960e7b271da4a6da14e12bc8a0ab515705c
Treshawn95 <treshawn95@yahoo.com>
2020-04-28 14:03:50 -0500

    error on link

--- VERSUS 5/VERSUS/AppDelegate.swift
+++ VERSUS 5/VERSUS/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+//import Firebase
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        // Override point for customization after application launch.
-        return true
+    //FirebaseApp.configure()
+    
+    return true
     }
 
     // MARK: UISceneSession Lifecycle
--- VERSUS 5/VERSUS/LoginViewController.swift
+++ VERSUS 5/VERSUS/LoginViewController.swift
@@ -25,92 +25,65 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         override func viewDidLoad()
         {
       super.viewDidLoad()
-            //setUpElements()
+            setUpElements()
     }
-    /*func setUpElements(){
+    func setUpElements(){
         //hide error label
         errorLabel.alpha = 0
         
         //style the elements
-        Utilities.styleTextField(userName)
-        Utilities.styleTextField(passWord)
-        Utilities.styleFilledButton(login)
+       // Utilities.styleTextField(userName)
+        //Utilities.styleTextField(passWord)
+        //Utilities.styleFilledButton(login)
         
-    }*/
+    }
     
 
     
     @IBAction func Logintapped(_ sender: Any) {
         
-        //if (filledOut() == false) {
-            //return
-        //}
-            let username =  userName.text!
-            let password = passWord.text!
-            let message = errorLabel.text!
-        
-        let url = URL(string: "https://vast-gorge-25891.herokuapp.com/login?username=\(userName))&password=\( passWord)")
+        let username =  userName.text
+                  let password = passWord.text
 
-        
-        guard let requestUrl = url else { fatalError() }
-
-                     //Create request for URL
-                     var request = URLRequest(url: requestUrl)
-
-                     //Specifying method to use for HTTP
-                     request.httpMethod = "GET"
-
-                     //Send HTTP request
-                     let task = URLSession.shared.dataTask(with: request) {
-                         (data, response, error) in
-
-                         //Check for errors
-                         if let error = error {
-                             print ("Error took place \(error)")
-                             return
-                         }
-                     //Reading HTTP response status code
-                         if let response = response as? HTTPURLResponse {
-                             print("Response HTTP Status code: \(response.statusCode)")
-                         }
-                      //Converting the response data of HTTP to string
-                          if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                          print("Response data string: \n \(dataString)")
-                              do {
-                                  let json = try JSONSerialization.jsonObject(with: data, options: [])
-                                  print(json)
-                              } catch {
-                                  print(error)
-                              }
-                           }
-
-                      }
-                     task.resume()
+                  if (username == "" || password == "")
+              {
+                  return
           }
+
+        dologin(user: username!, psw: password!)
+        
     }
-    
-        func displayAlert(msgTitle:String, msgContent:String){
-            let alertController = UIAlertController(title: msgTitle,message: msgContent, preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "Close", style: .default, handler: nil)
-            alertController.addAction(defaultAction)
-            
-           // present(alertController, animated: true, completion: nil)
+    func dologin( user:String, psw:String)
+    {
+        
+        let url = URL(string: "https://vast-gorge-25891.herokuapp.com/login?username=\(userName))&password=\( passWord)")!
+        // session = URLSession.shared
+        let request = NSMutableURLRequest(url:url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let response = response as? HTTPURLResponse {
+                    print("statusCode: \(response.statusCode)")
+                }
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("data: \(dataString)")
+                }
+            }
         }
-     
-            
-    
-    
-    
-
-    
-    
-
-
-  
-     //  errorlabel.text = "What is your favorite ice cream?"
+        task.resume()
+    }
+        
+        
+}
+           
 
         
         
     
+
+
 
 
